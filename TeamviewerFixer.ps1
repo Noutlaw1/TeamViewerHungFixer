7ad3105b23cab4d6daf194e0b2b38640e7199819
Nick Outlaw <nickoutlaw@gmail.com>
2017-04-23 12:14:42 -0400

    Add files via upload
    Basically, it just grabs every process that looks like it might be a Teamviewer process, kills it, then restarts the Teamviewer service.

---
+++ TeamviewerFixer.ps1
@@ -0,0 +1,14 @@
+﻿$Processes = Get-Process | Where {$_.Name -like "*tv*"}
+$Processes += Get-Process | Where {$_.Name -like "*Teamviewer*"}
+
+foreach ($Process in $Processes)
+    {
+        stop-process $process
+    }
+Write-host "Stopping Service."
+$Services = get-service | Where {$_.name -like "teamviewer"}
+foreach ($service in $Services)
+    {
+    Stop-Service $Service
+    Start-SErvice $Service
+    }
